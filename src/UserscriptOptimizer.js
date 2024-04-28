98453c4f69ee8ad83e310ae43cafcc87af83e4e4
yaseen <yaseen.nvt@gmail.com>
2024-04-27 21:22:51 -0400

    formatting, small refactors

--- src/UserscriptOptimizer.js
+++ src/UserscriptOptimizer.js
@@ -1,37 +1,32 @@
 const { Optimizer } = require('@parcel/plugin');
-const {
-  countLines,
-} = require('@parcel/utils');
+const { countLines } = require('@parcel/utils');
 const SourceMap = require('@parcel/source-map').default;
 
 
 
 module.exports = new Optimizer({
-    async loadConfig({config}) {
-      // we use toml since it has nicer syntax for multi-line strings than json
-      // and we dont want js since its overkill
-      let {contents, filePath} = await config.getConfig([
-        'userscript.toml' 
-      ]);
-      console.log(contents)
-      return contents.header;
+  async loadConfig({ config }) {
+    // we use toml since it has nicer syntax for multi-line strings than json
+    // and we dont want js since its overkill
+    let { contents, filePath } = await config.getConfig(['userscript.toml']);
+    config.invalidateOnFileChange(filePath) // not sure this has an effect on e.g. parcel watch
+    return contents;
   },
-  
+
   async optimize({
-    bundle,
     contents,
     map,
     options,
     config
   }) {
-    const header = config
+    const header = config.header
     const newMap = new SourceMap(options.projectRoot);
     if (options.sourceMaps) {
       const mapBuffer = map.toBuffer();
       const lineOffset = countLines(header) - 1;
       newMap.addBufferMappings(mapBuffer, lineOffset);
     }
-    
+
     return { contents: header + contents, map: newMap };
   },
 });
